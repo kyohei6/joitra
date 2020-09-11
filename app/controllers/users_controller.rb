28184de893cde899c4f2381e3e0384aa3f62585c
kyohei.uda <fcm1kyohei@gmail.com>
2020-09-11 09:47:47 +0000

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to login_url
+      redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
